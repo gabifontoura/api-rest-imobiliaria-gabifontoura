aad8f4ab152408e64330058eacd6b0d4c6bf3ca5
Gabriela <gabrielagarciafontoura@gmail.com>
2023-03-06 15:22:11 -0300

    feat:login

---
+++ src/interfaces/login.interfaces.ts
@@ -0,0 +1,5 @@
+import { z } from "zod";
+import { createLoginSchema } from "../schemas/login.schemas";
+
+
+export type tLogin = z.infer<typeof createLoginSchema>
--- src/schemas/users.schemas.ts
+++ src/schemas/users.schemas.ts
@@ -2,12 +2,11 @@ import { z } from 'zod'
 import { hashSync } from 'bcryptjs'
 
 
-
 export const userSchema = z.object({
     name: z.string().min(3).max(45),
     email: z.string().email().min(10).max(45),
     admin:z.boolean().default(false),
-    password: z.string().min(4).max(20).transform((pass) => {
+    password: z.string().min(4).max(120).transform((pass) => {
         return hashSync(pass, 10)
     })
 }).strip()
